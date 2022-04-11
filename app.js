1c8ed4da1b99a5f553dd5aedce9b65c20d461c0c
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-11 23:55:53 +0700

    delete: Cleaning up database and requests

--- app.js
+++ app.js
@@ -3,25 +3,13 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let sqlite3 = require('sqlite3').verbose()
 let bodyParser = require('body-parser')
-
-let db = new sqlite3.Database('test.db')
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let dataRouter = require('./routes/data');
-let dbRouter = require('./routes/db');
-
-let dbGetUsersRouter = require('./routes/db_get_users');
 
 let app = express();
-
-// db start up
-db.serialize(() => {
-	db.run('CREATE TABLE IF NOT EXISTS user (userid INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT NOT NULL, password TEXT)')
-	db.run('CREATE TABLE IF NOT EXISTS task (taskid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, content TEXT, userid INTEGER NOT NULL)')
-})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/express_backend', dataRouter);
-app.use('/db_test', dbRouter);
-app.use('/db_get_users', dbGetUsersRouter);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 let sqlite3 = require('sqlite3').verbose()
 
-const DBSOURCE = 'db.sqlite'
+const DBSOURCE = 'sqlite.db'
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
 	if (err) {
@@ -8,23 +8,13 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 		throw err;
 	} else {
 		console.log('Connected to the SQLite database.')
-		db.run(`CREATE TABLE user (
+		db.run(`CREATE TABLE IF NOT EXISTS user (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			name text, 
 			email text UNIQUE, 
 			password text, 
 			CONSTRAINT email_unique UNIQUE (email)
-		)`,
-		(err) => {
-			if (err) {
-				// Table already created
-			} else {
-				// Table just created, creating some rows
-				let insert = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`
-				db.run(insert, ["admin", "admin@example.com", "admin123456"])
-				db.run(insert, ["user", "user@example.com", "user123456"])
-			}
-		});
+		)`);
 	}
 });
 
